c03d7030b79197af2fbacb0b376956cd364468fb
PedroWernek <pedrowcnetto@gmail.com>
2023-11-16 23:15:19 -0300

    Versão inicial

---
+++ js/linhadotempo.js
@@ -0,0 +1,8 @@
+window.onload = () =>{
+
+
+btn.onclick = () =>{
+    alert("Porque você clicou?                                                                         De qualquer forma não faço nada, esse rombo no espaço tempo leva tempo pra ser consertado                                                                      Seja paciênte...");
+};
+
+}
